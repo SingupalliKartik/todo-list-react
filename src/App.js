76cdfe399f3468247412c72871b1a728a1974a5b
Singupalli Kartik <skartik1706@gmail.com>
2023-09-26 21:39:16 +0530

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './mycomponents/Header';
+import {Todo}  from './mycomponents/Todo';
+import {Todos}  from './mycomponents/Todos';
+import {Footer}  from './mycomponents/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+        <Header title= 
+      "My Todo List" searchbox={true} />
+        <Todos/>
+        <Footer/>
+      
+    </>
   );
 }
 
